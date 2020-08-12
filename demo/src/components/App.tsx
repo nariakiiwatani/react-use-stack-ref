5d27f8f2f729e02b142e2dd837d3b55b18c84143
nariakiiwatani <nariakiiwatani@annolab.com>
2020-08-12 16:45:30 +0900

    create demo

--- demo/src/components/App.tsx
+++ demo/src/components/App.tsx
@@ -1,9 +1,29 @@
 
-import React, { useState } from 'react'
-import { ModuleName } from 'your-package-name'
+import React, { useState, useCallback, ChangeEvent } from 'react'
+import { useStackRef } from 'react-use-stack-ref'
 
 const App = (props: {}) => {
-	return (<></>)
+	const stack = useStackRef()
+	const [input, setInput] = useState("")
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setInput(e.target.value)
+	}
+	const handlePush = useCallback(e => {
+		stack.push(input)
+		setInput("")
+	}, [stack])
+	const handlePop = useCallback(e => {
+		const value = stack.pop()
+		value && setInput(value as string)
+	}, [stack])
+	return (<>
+		<h1>live demo for useStackRef</h1>
+		<input type="text" value={input} onChange={handleChange} />
+		<button onClick={handlePush} disabled={input.length === 0}>push</button>
+		<button onClick={handlePop} disabled={stack.size === 0} >pop</button>
+		<div>current value: {stack.value}</div>
+		<div>stack depth: {stack.size}</div>
+	</>)
 }
 
 export default App;
